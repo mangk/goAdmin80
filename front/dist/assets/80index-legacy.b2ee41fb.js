1e44f99f0c3eddd6f9f5fd8ead593f6afb550522
mangk <mangk.l@qq.com>
2023-05-25 12:22:59 +0800

    增加系统提示功能

---
+++ front/dist/assets/80index-legacy.b2ee41fb.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on gin-vue-admin 
+ Time : 1684988314000 */
+System.register(["./80_plugin-vue_export-helper-legacy.c6aa7c23.js","./80index-legacy.bdc13888.js"],(function(e,n){"use strict";var r,t,i,s;return{setters:[function(e){r=e._},function(e){t=e.o,i=e.d,s=e.v}],execute:function(){var u=""+new URL("notFound-4e921f05.png",n.meta.url).href,c=""+new URL("qm-dd0442fb.png",n.meta.url).href,l={name:"Error"},p=[s('<div class="big"><div class="inner"><img src="'+u+'"><p>页面被神秘力量吸走了（如果您是开源版请联系我们修复）</p><p style="font-size:18px;line-height:40px;">常见问题为当前此角色无当前路由，如果确定要使用本路由，请到角色管理进行分配</p><p>↓</p><img src="'+c+'" class="leftPic"></div></div>',1)];e("default",r(l,[["render",function(e,n,r,s,u,c){return t(),i("div",null,p)}]]))}}}));
---
+++ front/dist/assets/80reload-legacy.c32edde7.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on gin-vue-admin 
+ Time : 1684988314000 */
+System.register(["./80index-legacy.bdc13888.js"],(function(e,t){"use strict";var n,u,r;return{setters:[function(e){n=e.u,u=e.o,r=e.d}],execute:function(){var t={name:"Reload"};e("default",Object.assign(t,{setup:function(e){return n().go(-1),function(e,t){return u(),r("div")}}}))}}}));
