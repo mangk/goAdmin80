47be1a478c80f4b18a9caa70a084e554f64144d4
mangk <mangk.l@qq.com>
2023-06-19 19:24:11 +0800

    更新文件上传组件

---
+++ front/dist/assets/80system-legacy.29210be9.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on gin-vue-admin 
+ Time : 1687172966000 */
+System.register(["./80index-legacy.afadea66.js"],(function(e,t){"use strict";var n;return{setters:[function(e){n=e.ah}],execute:function(){e("_",""+new URL("goAdmin80-9d38979c.png",t.meta.url).href),e("s",(function(){return n({url:"/info",method:"get"})}))}}}));
