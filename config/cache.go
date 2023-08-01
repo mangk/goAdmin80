2875cebfc0b13ecdf7e1089a71315760f2135b82
mangk <mangk.l@qq.com>
2023-08-01 17:58:32 +0800

    新结构

--- config/cache.go
+++ config/cache.go
@@ -1,11 +1,7 @@
 package config
 
-type Redis struct {
+type Cache struct {
 	DB       int    `json:"db" yaml:"db"`             // 数据库index
 	Addr     string `json:"addr" yaml:"addr"`         // 服务器地址:端口
 	Password string `json:"password" yaml:"password"` // 密码
-}
-
-type Cache struct {
-	Redis
 }
