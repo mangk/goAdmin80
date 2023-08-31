de5b4d3ea13b6e98140a58ed781b20721a29ac1e
mangk <mangk.l@qq.com>
2023-08-31 10:42:40 +0800

    更新基础model

--- model/model.go
+++ model/model.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"database/sql/driver"
+	"fmt"
 	"time"
 
 	"gorm.io/gorm"
@@ -72,5 +74,44 @@ func Find(tx *gorm.DB) (data []map[string]interface{}, err error) {
 		data = append(data, entry)
 	}
 
+	return
+}
+
+const TimeFormat = "2006-01-02 15:04:05"
+
+type LocalTime time.Time
+
+func (t LocalTime) MarshalJSON() ([]byte, error) {
+	tTime := time.Time(t)
+	return []byte(fmt.Sprintf("\"%v\"", tTime.Format("2006-01-02 15:04:05"))), nil
+}
+func (t LocalTime) Value() (driver.Value, error) {
+	var zeroTime time.Time
+	tlt := time.Time(t)
+	//判断给定时间是否和默认零时间的时间戳相同
+	if tlt.UnixNano() == zeroTime.UnixNano() {
+		return nil, nil
+	}
+	return tlt, nil
+}
+
+func (t *LocalTime) Scan(v interface{}) error {
+	if value, ok := v.(time.Time); ok {
+		*t = LocalTime(value)
+		return nil
+	}
+	return fmt.Errorf("can not convert %v to timestamp", v)
+}
+
+func (t *LocalTime) UnmarshalJSON(data []byte) (err error) {
+	// 空值不进行解析
+	if len(data) == 2 {
+		*t = LocalTime(time.Time{})
+		return
+	}
+
+	// 指定解析的格式
+	now, err := time.Parse(`"`+TimeFormat+`"`, string(data))
+	*t = LocalTime(now)
 	return
 }
